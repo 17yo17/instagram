a440eb981037c31418bf23591a3db6bf857f0f9c
Ryo Taono <taonoryo0217@gmail.com>
2024-07-25 11:44:24 -0700

    Designed the follower lists on profile page and notification tab

---
+++ Instagram/View/Cells/Notifications/NotificationFollowEventTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  NotificationFollowEventTableViewCell.swift
+//  Instagram
+//
+//  Created by 垰野亮 on 7/25/24.
+//
+
+import UIKit
+
+class NotificationFollowEventTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
